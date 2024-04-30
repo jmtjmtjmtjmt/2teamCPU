a11dcdcabd8f803ab634cb65124c169cb37fa484
J-HanRyang <162584369+J-HanRyang@users.noreply.github.com>
2024-04-30 15:03:21 +0900

    Update Adder.v

--- Adder.v
+++ Adder.v
@@ -8,7 +8,7 @@ module twos_adder(x, y, c_in, sum, c_out);
 	wire     [N-1:0] t;
 
   assign t = y ^ {N{c_in}};            // c_in이 0이면 Add, 1이면 Sub
-  assign {c_out, sum} = x + t + c_in;  // c_out이 0이면 덧셈 결과, 1이면 뺄셈 결과 >> 덧셈에서1, 뺌셈에서0이 나오면 Overflow발생
+  assign {c_out, sum} = x + t + c_in;  // c_out이 0이면 덧셈 결과(양수), 1이면 뺄셈 결과(음수) >> 덧셈에서1, 뺄셈에서0이 나오면 Overflow발생
 endmodule
 
 
